1d2dc87b96c34b889f5bd3670374c9fb9715d0a5
LKingJ23 <lkingj23@gmail.com>
2018-03-28 13:19:36 +0200

    Image Validation, Navigation, Default Image, Auto DateField

--- mysite/baseItems/forms.py
+++ mysite/baseItems/forms.py
@@ -8,6 +8,22 @@ class NewsForm(forms.ModelForm):
         model = New
         fields = ['title', 'subtitle', 'body', 'image']
 
+    def clean_image(self):
+        cleaned_data = super(NewsForm, self).clean()
+        image = cleaned_data.get('image')
+
+        if image:
+            image_name = image.name
+            image_split = image_name.split('.')
+            image_format = image_split[-1]
+            formats = ['jpg', 'jpeg', 'JPG', 'JPEG', 'png', 'PNG']
+            if image_format not in formats:
+                raise forms.ValidationError("Wrong format, only jpg, jpeg, pnp")
+            elif image.size > 10 * 1024 * 1024:
+                raise forms.ValidationError("Too heavy image, less than 10 MB")
+            else:
+                return image
+
 
 class ImageUploadForm(forms.Form):
     image = forms.ImageField()
