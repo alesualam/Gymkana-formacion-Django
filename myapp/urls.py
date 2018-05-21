a3f4db95019c1abe0fade0347c09e978ce497e03
alesualam <alejandro7c@gmail.com>
2018-05-21 16:51:52 +0200

    Tarea formulario

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^v1/news/create/', views.create, name='create'),
+    url(r'v1/news/create/', views.create, name='create'),
 ]
