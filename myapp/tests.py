bdcc44b9fee232ccd39b83b248330b7f2e828a31
alesualam <alejandro7c@gmail.com>
2018-05-24 11:09:25 +0200

    NEWS fixed v3

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from myapp.models import New, Event
+from django.http import HttpRequest
+
+from myapp import views
+from myapp import urls
+
+class IndexTests(TestCase):
+
+    def test_index_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
